375668d86bbcd2f855838e582c2c2dc3bb2bfd56
Tomik Tomikyan <tomik.tomikyan.g@gmail.com>
2025-06-30 01:15:56 +0400

    Make the table container wider

--- src/components/ClassGroups.tsx
+++ src/components/ClassGroups.tsx
@@ -368,7 +368,7 @@ const ClassGroups: React.FC<ClassGroupsProps> = ({
   };
 
   return (
-    <div className="max-w-6xl mx-auto space-y-6">
+    <div className="max-w-full mx-auto space-y-6">
       {/* Header */}
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-3">
